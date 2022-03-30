f0a8ddcdefa3d2b2be520b7ff787a7db676a718d
arjun kaushik <arjunkaushik810@gmail.com>
2022-03-30 14:08:33 +0530

    kSum

--- src/Tree/printKSumPath.java
+++ src/Tree/printKSumPath.java
@@ -1,8 +1,8 @@
 package Tree;
 
-import java.util.*;
+import java.util.ArrayList;
 
-public class preOrderTreversal {
+public class printKSumPath {
     static class Node{
         int data;
         Node left;
@@ -11,18 +11,26 @@ public class preOrderTreversal {
             data = k;
         }
     }
+    static ArrayList<Integer> arr = new ArrayList<>();
 
-    static Vector<Integer> arr = new Vector<Integer>();
-
-    public static void preorder(Node node) {
-        if (node == null) return;
-        arr.add(node.data);
-        System.out.println(node.data);
-        preorder(node.left);
-        preorder(node.right);
-        System.out.println(arr);
+    public static void print(ArrayList<Integer> arr, int i) {
+        for (int j=arr.size()-1;j>=i;j--)
+            System.out.print(arr.get(j)+" ");
+        System.out.println();
+    }
+    public static void printSum(Node root,int k) {
+        if (root == null)return;
+        arr.add(root.data);
+        printSum(root.left,k);
+        printSum(root.right,k);
+        int temp = 0;
+        for (int i= arr.size()-1;i>=0;i--){
+                temp+=arr.get(i);
+                if (temp==k) print(arr,i);
+        }
         arr.remove(arr.size()-1);
     }
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(3);
@@ -35,8 +43,7 @@ public class preOrderTreversal {
         root.right.left.right = new Node(2);
         root.right.right = new Node(5);
         root.right.right.right = new Node(2);
-        System.out.println("Pree order taversal = ");
-        preorder(root);
-
+        int k = 5;
+        printSum(root,k);
     }
 }
